3bcfedbc13da0901ba16bac40dfb76aee48503d2
EZavdovev <45925246+EZavdovev@users.noreply.github.com>
2019-05-26 20:41:01 +0200

    Add files via upload

--- Stack/Stack/mutex_stack.h
+++ Stack/Stack/mutex_stack.h
@@ -1,15 +1,38 @@
 #pragma once
 #include<stack>
 #include<mutex>
+template<typename T>
 class mutex_stack{
 private:
-	std::stack<int> database;
+	std::stack<T> database;
 	std::mutex m;
 
 public:
 	mutex_stack();
-	void push(int data);
-	int top_and_pop();
+	void push(const T& data);
+	T top_and_pop();
 	~mutex_stack();
 };
+
+template<typename T>
+mutex_stack<T>::mutex_stack() {
+}
+template<typename T>
+void mutex_stack<T>::push(const T& data) {
+	std::lock_guard<std::mutex> lock(m);
+	database.push(data);
+}
+template<typename T>
+T mutex_stack<T>::top_and_pop() {
+	std::lock_guard<std::mutex> lock(m);
+	T ans = -1;
+	if (!database.empty()) {
+		ans = database.top();
+		database.pop();
+	}
+	return ans;
+}
+template<typename T>
+mutex_stack<T>::~mutex_stack() {
+}
 
